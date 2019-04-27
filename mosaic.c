7fcee897561c36ed80ca7a55523471e912b47109
luizedsilva <luizedsilva@gmail.com>
2019-04-27 19:02:06 -0300

    Improved mosaic.c code

--- mosaic.c
+++ mosaic.c
@@ -1,7 +1,31 @@
 /*-------------------------------------------------------------------------
  * Image Processing using C-Ansi
- *   Program: Create photomosaic from images
+ *   Program to create photomosaic from images
+ * 
  * By Luiz Eduardo da Silva.
+ * 
+ * Use 'config.txt' file which should be organized as follows:
+ *  + The first line of the file must contain three numbers that define
+ *    the NTONES number of grayscale tiles, the NROWS number of rows
+ *    and the NCOLS number of mosaic columns and images that will be
+ *    used for this composition.
+ *  + The second line contains the NFILES number of images that will be 
+ *    used for mounting.
+ *  + From the third line onwards the file contains the names of the
+ *    NFILES images that will be used in the assembly. The first image of 
+ *    the list is also used as base image, ie all images will be iconized 
+ *    and darkened or cleared to be used as 'pixels' for mounting the first 
+ *    image.
+ * 
+ * Example:
+ * --------
+ *   32 60 80
+ *   5
+ *   image1.pgm
+ *   image2.pgm
+ *   image3.pgm
+ *   image4.pgm
+ *   image5.pgm
  *-------------------------------------------------------------------------*/
 
 #include <stdio.h>
@@ -10,56 +34,46 @@
 #include <time.h>
 #include "imagelib.h"
 
+#define ERROR(x, y) \
+    if (x)          \
+    y
+
 void iconize(image In, image Out, int nl, int nc, int mn, int ny, int nx)
 {
     int i, j;
-    int py = nl / (ny - 1);
-    int px = nc / (nx - 1);
     for (i = 0; i < ny; i++)
         for (j = 0; j < nx; j++)
-        {
-            Out[i * nx + j] = In[i * py * nc + j * px];
-        }
+            Out[i * nx + j] = In[i * (nl / (ny - 1)) * nc + j * nc / (nx - 1)];
 }
 
 int calctone(image In, int nl, int nc, int mn, int ntones)
 {
-    int i, j, median, sum, value, scaled;
-    for (i = 0, sum = 0; i < nl; i++)
-        for (j = 0; j < nc; j++)
-        {
-            sum += In[i * nc + j];
-        }
-    median = sum / (nl * nc);
-    return ((float)median / mn) * ntones;
+    int i, sum;
+    for (i = 0, sum = 0; i < nl * nc; i++)
+        sum += In[i];
+    return ((float)sum / (nl * nc) / mn) * ntones;
 }
 
 void createimgtone(image In, image Out, int nl, int nc, int mn, int value)
 {
-    int i, j, sum;
-    for (i = 0; i < nl; i++)
-        for (j = 0; j < nc; j++)
-        {
-            int sum = value + In[i * nc + j];
-            if (sum > 255)
-                sum = 255;
-            if (sum < 0)
-                sum = 0;
-            Out[i * nc + j] = sum;
-        }
+    int i;
+    for (i = 0; i < nl * nc; i++)
+    {
+        int sum = value + In[i];
+        sum = sum > 255 ? 255 : sum;
+        sum = sum < 0 ? 0 : sum;
+        Out[i] = sum;
+    }
 }
 
-void error(char *s)
+void msg(char *s)
 {
     puts(s);
     exit(10);
 }
 
-/*-------------------------------------------------------------------------
- * main function
- *-------------------------------------------------------------------------*/
-
-typedef struct {
+typedef struct
+{
     int nrows, ncols, maxl;
     char *filename;
     image icon;
@@ -67,6 +81,9 @@ typedef struct {
 
 typedef imgtype *ptimg;
 
+/*-------------------------------------------------------------------------
+ * main function
+ *-------------------------------------------------------------------------*/
 int main(void)
 {
     FILE *arq;
@@ -74,82 +91,61 @@ int main(void)
     char name[102], command[100];
     image *imgtone, In, Out;
     ptimg imginfo;
-
-   /*-------------------------------------------
+    /*-------------------------------------------
     * Read data from config.txt
     *-------------------------------------------*/
-    if ((arq = fopen("config.txt", "rt")) == NULL)
-        error("File <config.txt> not found!");
+    ERROR((arq = fopen("config.txt", "rt")) == NULL, msg("File <config.txt> not found!"));
     fscanf(arq, "%d %d %d", &ntones, &nrows, &ncols);
     fscanf(arq, "%d ", &nfiles);
-
-    if (nfiles > 20)
-        error("Max. files number exceeded!");
-
-    imginfo = malloc (nfiles * sizeof(imgtype));
-    if (!imginfo)
-        error("Image info structure creation error!");
-
+    ERROR(nfiles > 20, msg("Maximum files number exceeded!"));
+    imginfo = malloc(nfiles * sizeof(imgtype));
+    ERROR(!imginfo, msg("Image info structure creation error!"));
     for (i = 0; i < nfiles; i++)
     {
         fscanf(arq, "%s", name);
-        imginfo[i].filename = malloc(strlen(name) * sizeof(char));
+        imginfo[i].filename = malloc(strlen(name) * sizeof(char) + 1);
         strcpy(imginfo[i].filename, name);
-        //puts(name);
     }
     fclose(arq);
-
-   /*-------------------------------------------
+    /*-------------------------------------------
     * Iconize images
     *-------------------------------------------*/
-
     for (i = 0; i < nfiles; i++)
     {
         In = img_readpgm(imginfo[i].filename, &imginfo[i].nrows, &imginfo[i].ncols, &imginfo[i].maxl);
-        //puts (imginfo[i].filenames);
-        if (!In)
-            error("Image read pgm error!");
+        ERROR(!In, msg("PGM Image read error!"));
         imginfo[i].icon = img_alloc(nrows, ncols);
-        if (!imginfo[i].icon)
-            error("Icon image creation error!");
+        ERROR(!imginfo[i].icon, msg("Icon image creation error!"));
         iconize(In, imginfo[i].icon, imginfo[i].nrows, imginfo[i].ncols, imginfo[i].maxl, nrows, ncols);
         img_free(&In);
     }
-
-   /*-------------------------------------------
+    /*-------------------------------------------
     * Create images of tones
     *-------------------------------------------*/
     srand((unsigned)time(NULL));
     imgtone = malloc(ntones * sizeof(image));
-    if (!imgtone)
-        error("Tone image creation error!");
+    ERROR(!imgtone, msg("Tone's image vector creation error!"));
     for (tone = 0; tone < ntones; tone++)
     {
-        int isorted = rand() % (nfiles-1) + 1; // skip de fisrt image
-        //printf ("Icon sorted = %d\n", iconsorted);
-        int tonecalculated = calctone(imginfo[isorted].icon, nrows, ncols, imginfo[isorted].maxl, ntones+1);
-        int value = (tone - tonecalculated) * imginfo[isorted].maxl/ (ntones+1);
+        int isorted = rand() % (nfiles - 1) + 1; // skip de fisrt image
+        int tonecalculated = calctone(imginfo[isorted].icon, nrows, ncols, imginfo[isorted].maxl, ntones + 1);
+        int value = (tone - tonecalculated) * imginfo[isorted].maxl / (ntones + 1);
         imgtone[tone] = img_alloc(nrows, ncols);
-        if (!imgtone[tone])
-            error("Tone image creation error!");
+        ERROR(!imgtone[tone], msg("Tone image creation error!"));
         createimgtone(imginfo[isorted].icon, imgtone[tone], nrows, ncols, imginfo[isorted].maxl, value);
     }
-
-   /*-------------------------------------------
+    /*-------------------------------------------
     * Create mosaic
     *-------------------------------------------*/
     Out = img_alloc(nrows * nrows, ncols * ncols);
-    if (!Out)
-        error("Out image creation error!");
+    ERROR(!Out, msg("Out image creation error!"));
     for (i = 0; i < nrows; i++)
         for (j = 0; j < ncols; j++)
         {
-            tone = (float)imginfo[0].icon[i * ncols + j]/ imginfo[0].maxl * (ntones-1); // pixels of first image listed
-            //printf ("%2d", tone);
-            if (tone < 0 || tone >= ntones)
-                error ("Range tones image error!");
+            tone = (float)imginfo[0].icon[i * ncols + j] / imginfo[0].maxl * (ntones - 1); // pixels of first image listed
+            ERROR(tone < 0 || tone >= ntones, msg("Tone range error!"));
             for (y = 0; y < nrows; y++)
-                for (x = 0; x < ncols; x++) 
+                for (x = 0; x < ncols; x++)
                     Out[(i * nrows + y) * ncols * ncols + j * ncols + x] = imgtone[tone][y * ncols + x];
         }
     sprintf(name, "%s-mosaic.pgm", imginfo[0].filename);
